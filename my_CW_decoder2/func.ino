99ea6da1e901e17362b318006838ff2cf9aee1dc
L7Cy <l7cy.at@gmail.com>
2022-06-23 15:04:11 +0900

    和文への対応

--- my_CW_decoder2/func.ino
+++ my_CW_decoder2/func.ino
@@ -31,131 +31,302 @@ void calcavg()
 
 void decode()
 {
-  if (strcmp(code, ".-") == 0)
-    printascii(65);
-  if (strcmp(code, "-...") == 0)
-    printascii(66);
-  if (strcmp(code, "-.-.") == 0)
-    printascii(67);
-  if (strcmp(code, "-..") == 0)
-    printascii(68);
-  if (strcmp(code, ".") == 0)
-    printascii(69);
-  if (strcmp(code, "..-.") == 0)
-    printascii(70);
-  if (strcmp(code, "--.") == 0)
-    printascii(71);
-  if (strcmp(code, "....") == 0)
-    printascii(72);
-  if (strcmp(code, "..") == 0)
-    printascii(73);
-  if (strcmp(code, ".---") == 0)
-    printascii(74);
-  if (strcmp(code, "-.-") == 0)
-    printascii(75);
-  if (strcmp(code, ".-..") == 0)
-    printascii(76);
-  if (strcmp(code, "--") == 0)
-    printascii(77);
-  if (strcmp(code, "-.") == 0)
-    printascii(78);
-  if (strcmp(code, "---") == 0)
-    printascii(79);
-  if (strcmp(code, ".--.") == 0)
-    printascii(80);
-  if (strcmp(code, "--.-") == 0)
-    printascii(81);
-  if (strcmp(code, ".-.") == 0)
-    printascii(82);
-  if (strcmp(code, "...") == 0)
-    printascii(83);
-  if (strcmp(code, "-") == 0)
-    printascii(84);
-  if (strcmp(code, "..-") == 0)
-    printascii(85);
-  if (strcmp(code, "...-") == 0)
-    printascii(86);
-  if (strcmp(code, ".--") == 0)
-    printascii(87);
-  if (strcmp(code, "-..-") == 0)
-    printascii(88);
-  if (strcmp(code, "-.--") == 0)
-    printascii(89);
-  if (strcmp(code, "--..") == 0)
-    printascii(90);
-
-  if (strcmp(code, ".----") == 0)
-    printascii(49);
-  if (strcmp(code, "..---") == 0)
-    printascii(50);
-  if (strcmp(code, "...--") == 0)
-    printascii(51);
-  if (strcmp(code, "....-") == 0)
-    printascii(52);
-  if (strcmp(code, ".....") == 0)
-    printascii(53);
-  if (strcmp(code, "-....") == 0)
-    printascii(54);
-  if (strcmp(code, "--...") == 0)
-    printascii(55);
-  if (strcmp(code, "---..") == 0)
-    printascii(56);
-  if (strcmp(code, "----.") == 0)
-    printascii(57);
-  if (strcmp(code, "-----") == 0)
-    printascii(48);
-
-  if (strcmp(code, "..--..") == 0)
-    printascii(63);
-  if (strcmp(code, ".-.-.-") == 0)
-    printascii(46);
-  if (strcmp(code, "--..--") == 0)
-    printascii(44);
-  if (strcmp(code, "-.-.--") == 0)
-    printascii(33);
-  if (strcmp(code, ".--.-.") == 0)
-    printascii(64);
-  if (strcmp(code, "---...") == 0)
-    printascii(58);
-  if (strcmp(code, "-....-") == 0)
-    printascii(45);
-  if (strcmp(code, "-..-.") == 0)
-    printascii(47);
-
-  if (strcmp(code, "-.--.") == 0)
-    printascii(40);
-  if (strcmp(code, "-.--.-") == 0)
-    printascii(41);
-  if (strcmp(code, ".-...") == 0)
-    printascii(95);
-  if (strcmp(code, "...-..-") == 0)
-    printascii(36);
-  if (strcmp(code, "...-.-") == 0)
-    printascii(62);
-  if (strcmp(code, ".-.-.") == 0) // AR
+  if(wabun==false)
   {
-    printascii(32);
-    printascii(65);
-    printascii(82);
-    Serial.println(wpm);
-    Serial.println();
-    charcount = 0;
+    //a to z
+    if (strcmp(code, ".-") == 0)
+      printascii(65);
+    if (strcmp(code, "-...") == 0)
+      printascii(66);
+    if (strcmp(code, "-.-.") == 0)
+      printascii(67);
+    if (strcmp(code, "-..") == 0)
+      printascii(68);
+    if (strcmp(code, ".") == 0)
+      printascii(69);
+    if (strcmp(code, "..-.") == 0)
+      printascii(70);
+    if (strcmp(code, "--.") == 0)
+      printascii(71);
+    if (strcmp(code, "....") == 0)
+      printascii(72);
+    if (strcmp(code, "..") == 0)
+      printascii(73);
+    if (strcmp(code, ".---") == 0)
+      printascii(74);
+    if (strcmp(code, "-.-") == 0)
+      printascii(75);
+    if (strcmp(code, ".-..") == 0)
+      printascii(76);
+    if (strcmp(code, "--") == 0)
+      printascii(77);
+    if (strcmp(code, "-.") == 0)
+      printascii(78);
+    if (strcmp(code, "---") == 0)
+      printascii(79);
+    if (strcmp(code, ".--.") == 0)
+      printascii(80);
+    if (strcmp(code, "--.-") == 0)
+      printascii(81);
+    if (strcmp(code, ".-.") == 0)
+      printascii(82);
+    if (strcmp(code, "...") == 0)
+      printascii(83);
+    if (strcmp(code, "-") == 0)
+      printascii(84);
+    if (strcmp(code, "..-") == 0)
+      printascii(85);
+    if (strcmp(code, "...-") == 0)
+      printascii(86);
+    if (strcmp(code, ".--") == 0)
+      printascii(87);
+    if (strcmp(code, "-..-") == 0)
+      printascii(88);
+    if (strcmp(code, "-.--") == 0)
+      printascii(89);
+    if (strcmp(code, "--..") == 0)
+      printascii(90);
+
+    //0 to 9
+    if (strcmp(code, ".----") == 0)
+      printascii(49);
+    if (strcmp(code, "..---") == 0)
+      printascii(50);
+    if (strcmp(code, "...--") == 0)
+      printascii(51);
+    if (strcmp(code, "....-") == 0)
+      printascii(52);
+    if (strcmp(code, ".....") == 0)
+      printascii(53);
+    if (strcmp(code, "-....") == 0)
+      printascii(54);
+    if (strcmp(code, "--...") == 0)
+      printascii(55);
+    if (strcmp(code, "---..") == 0)
+      printascii(56);
+    if (strcmp(code, "----.") == 0)
+      printascii(57);
+    if (strcmp(code, "-----") == 0)
+      printascii(48);
+
+    if (strcmp(code, "..--..") == 0)//'?'
+      printascii(63);
+    if (strcmp(code, ".-.-.-") == 0)//'.'
+      printascii(46);
+    if (strcmp(code, "--..--") == 0)//','
+      printascii(44);
+    if (strcmp(code, "-.-.--") == 0)//''
+      printascii(33);
+    if (strcmp(code, ".--.-.") == 0)//'@'
+      printascii(64);
+    if (strcmp(code, "---...") == 0)//':'
+      printascii(58);
+    if (strcmp(code, "-....-") == 0)//'='
+      printascii(45);
+    if (strcmp(code, "-..-.") == 0)
+      printascii(47);
+
+    if (strcmp(code, "-.--.") == 0)//'('
+      printascii(40);
+    if (strcmp(code, "-.--.-") == 0)//')'
+      Serial.write(")");
+    if (strcmp(code, ".-..-.") == 0)//和文にもどるやつ
+    {
+      Serial.write(")");
+      wabun=true;
+    }
+    if (strcmp(code, ".-...") == 0)
+      printascii(95);
+    if (strcmp(code, "...-..-") == 0)
+      printascii(36);
+    if (strcmp(code, "...-.-") == 0)
+      printascii(62);
+    if (strcmp(code, "...-.") == 0)
+      printascii(126);
+    if (strcmp(code, ".-.-.") == 0) // AR
+    {
+      printascii(32);
+      printascii(65);
+      printascii(82);
+      Serial.println(wpm);
+      Serial.println();
+      charcount = 0;
+    }
+
+    if (strcmp(code, "........") == 0) // HH
+    {
+      printascii(72);
+      printascii(72);
+    }
+    if (strcmp(code, "-...-") == 0) // BT
+    {
+      printascii(32);
+      printascii(66);
+      printascii(84);
+      Serial.println(wpm);
+      charcount = 0;
+    }
   }
-  if (strcmp(code, "...-.") == 0)
-    printascii(126);
+  else
+  {
+    //アからン
+    if (strcmp(code, "--.--") == 0) //"ア"
+      Serial.write("ア");
+    if (strcmp(code, ".-") == 0) //"イ"
+      Serial.write("イ");
+    if (strcmp(code, "..-") == 0) //"ウ"
+      Serial.write("ウ");
+    if (strcmp(code, "-.---") == 0) //"エ"
+      Serial.write("エ");
+    if (strcmp(code, ".-...") == 0) //"オ"
+      Serial.write("オ");
+    if (strcmp(code, ".-..") == 0) //"カ"
+      Serial.write("カ");
+    if (strcmp(code, "-.-..") == 0) //"キ"
+      Serial.write("キ");
+    if (strcmp(code, "...-") == 0) //"ク"
+      Serial.write("ク");
+    if (strcmp(code, "-.--") == 0) //"ケ"
+      Serial.write("ケ");
+    if (strcmp(code, "----") == 0) //"コ"
+      Serial.write("コ");
+    if (strcmp(code, "-.-.-") == 0) //"サ"
+      Serial.write("サ");
+    if (strcmp(code, "--.-.") == 0) //"シ"
+      Serial.write("シ");
+    if (strcmp(code, "---.-") == 0) //"ス"
+      Serial.write("ス");
+    if (strcmp(code, ".---.") == 0) //"セ"
+      Serial.write("セ");
+    if (strcmp(code, "---.") == 0) //"ソ"
+      Serial.write("ソ");
+    if (strcmp(code, "-.") == 0) //"タ"
+      Serial.write("タ");
+    if (strcmp(code, "..-.") == 0) //"チ"
+      Serial.write("チ");
+    if (strcmp(code, ".--.") == 0) //"ツ"
+      Serial.write("ツ");
+    if (strcmp(code, ".-.--") == 0) //"テ"
+      Serial.write("テ");
+    if (strcmp(code, "..-..") == 0) //"ト"
+      Serial.write("ト");
+    if (strcmp(code, ".-.") == 0) //"ナ"
+      Serial.write("ナ");
+    if (strcmp(code, "-.-.") == 0) //"ニ"
+      Serial.write("ニ");
+    if (strcmp(code, "....") == 0) //"ヌ"
+      Serial.write("ヌ");
+    if (strcmp(code, "--.-") == 0) //"ネ"
+      Serial.write("ネ");
+    if (strcmp(code, "..--") == 0) //"ノ"
+      Serial.write("ノ");
+    if (strcmp(code, "-...") == 0) //"ハ"
+      Serial.write("ハ");
+    if (strcmp(code, "--..-") == 0) //"ヒ"
+      Serial.write("ヒ");
+    if (strcmp(code, "--..") == 0) //"フ"
+      Serial.write("フ");
+    if (strcmp(code, ".") == 0) //"ヘ"
+      Serial.write("ヘ");
+    if (strcmp(code, "-..") == 0) //"ホ"
+      Serial.write("ホ");
+    if (strcmp(code, "-..-") == 0) //"マ"
+      Serial.write("マ");
+    if (strcmp(code, "..-.-") == 0) //"ミ"
+      Serial.write("ミ");
+    if (strcmp(code, "-") == 0) //"ム"
+      Serial.write("ム");
+    if (strcmp(code, "-...-") == 0) //"メ"
+      Serial.write("メ");
+    if (strcmp(code, "-..-.") == 0) //"モ"
+      Serial.write("モ");
+    if (strcmp(code, ".--") == 0) //"ヤ"
+      Serial.write("ヤ");
+    if (strcmp(code, "-..--") == 0) //"ユ"
+      Serial.write("ユ");
+    if (strcmp(code, "--") == 0) //"ヨ"
+      Serial.write("ヨ");
+    if (strcmp(code, "...") == 0) //"ラ"
+      Serial.write("ラ");
+    if (strcmp(code, "--.") == 0) //"リ"
+      Serial.write("リ");
+    if (strcmp(code, "-.--.") == 0) //"ル"
+      Serial.write("ル");
+    if (strcmp(code, "---") == 0) //"レ"
+      Serial.write("レ");
+    if (strcmp(code, ".-.-") == 0) //"ロ"
+      Serial.write("ロ");
+    if (strcmp(code, "-.-") == 0) //"ワ"
+      Serial.write("ワ");
+    // if (strcmp(code, ".-..-") == 0) //"ヰ"
+    //   printascii();
+    if (strcmp(code, ".---") == 0) //"ヲ"
+      Serial.write("ヲ");
+    // if (strcmp(code, ".--..") == 0) //"ヱ"
+    //   printascii();
+    if (strcmp(code, ".-.-.") == 0) //"ン"
+      Serial.write("ン");
 
-  if (strcmp(code, "........") == 0) // HH
-  {
-    printascii(72);
-    printascii(72);
-  }
-  if (strcmp(code, "-...-") == 0) // BT
-  {
-    printascii(32);
-    printascii(66);
-    printascii(84);
-    Serial.println(wpm);
-    charcount = 0;
+    // 0 to 9
+    if (strcmp(code, "-----") == 0)
+      printascii(48);
+    if (strcmp(code, ".----") == 0)
+      printascii(49);
+    if (strcmp(code, "..---") == 0)
+      printascii(50);
+    if (strcmp(code, "...--") == 0)
+      printascii(51);
+    if (strcmp(code, "....-") == 0)
+      printascii(52);
+    if (strcmp(code, ".....") == 0)
+      printascii(53);
+    if (strcmp(code, "-....") == 0)
+      printascii(54);
+    if (strcmp(code, "--...") == 0)
+      printascii(55);
+    if (strcmp(code, "---..") == 0)
+      printascii(56);
+    if (strcmp(code, "----.") == 0)
+      printascii(57);
+
+    if (strcmp(code, "..") == 0) //"濁点"
+      Serial.write("゛");
+    if (strcmp(code, "..--.") == 0) //"半濁点"
+      Serial.write("゜");
+    if (strcmp(code, "-.--.-") == 0) //"("
+    {
+      Serial.write("(");
+      wabun = false;
+    }
+    // if (strcmp(code, ".-..-.") == 0) //")"
+    // {
+    //   Serial.write(")");
+    //   wabun = true;
+    // }
+    if (strcmp(code, ".-.-.-") == 0) //"、"
+      Serial.write("、");
+    if (strcmp(code, "-..---") == 0) // AR
+    {
+      printascii(32);
+      printascii(65);
+      printascii(82);
+      Serial.println(wpm);
+      Serial.println();
+      charcount = 0;
+    }
+    if (strcmp(code, "...-.") == 0) // HH
+    {
+      printascii(72);
+      printascii(72);
+    }
+    if (strcmp(code, "..--..") == 0) // BT
+    {
+      printascii(32);
+      printascii(66);
+      printascii(84);
+      Serial.println(wpm);
+      charcount = 0;
+    }
   }
   code[0] = '\0';
 }
@@ -163,35 +334,6 @@ void decode()
 void printascii(int asciinumber)
 {
   Serial.write(asciinumber);
-
-  // int fail = 0;
-  // if (rows == 4 and colums == 16)
-  //   fail = -4; /// to fix the library problem with 4*16 display http://forum.arduino.cc/index.php/topic,14604.0.html
-
-  // if (lcdindex > colums - 1)//端までいったら
-  // {
-  //   lcdindex = 0;
-  //   if (rows == 4)
-  //   {
-  //     for (int i = 0; i <= colums - 1; i++)
-  //     {
-  //       lcd.setCursor(i, rows - 3);
-  //       lcd.write(line2[i]);
-  //       line2[i] = line1[i];
-  //     }
-  //   }
-  //   for (int i = 0; i <= colums - 1; i++)
-  //   {
-  //     lcd.setCursor(i + fail, rows - 2);
-  //     lcd.write(line1[i]);
-  //     lcd.setCursor(i + fail, rows - 1);
-  //     lcd.write(32);
-  //   }
-  // }
-  // line1[lcdindex] = asciinumber;
-  // lcd.setCursor(lcdindex + fail, rows - 1);
-  // lcd.write(asciinumber);
-  // lcdindex += 1;
 }
 
 // void checklacktime()
@@ -202,33 +344,4 @@ void printascii(int asciinumber)
 //     lacktime = 1.2;
 //   if (wpm > 35)
 //     lacktime = 1.5;
-// }
-
-// void updateinfolinelcd()
-// {
-//   int place;
-//   if (rows == 4)
-//   {
-//     place = colums / 2;
-//   }
-//   else
-//   {
-//     place = 2;
-//   }
-//   if (wpm < 10)
-//   {
-//     lcd.setCursor((place)-2, 0);
-//     lcd.print("0");
-//     lcd.setCursor((place)-1, 0);
-//     lcd.print(wpm);
-//     lcd.setCursor((place), 0);
-//     lcd.print(" WPM");
-//   }
-//   else
-//   {
-//     lcd.setCursor((place)-2, 0);
-//     lcd.print(wpm);
-//     lcd.setCursor((place), 0);
-//     lcd.print(" WPM ");
-//   }
 // }
--- my_CW_decoder2/my_CW_decoder2.ino
+++ my_CW_decoder2/my_CW_decoder2.ino
@@ -1,14 +1,3 @@
-// #include <LiquidCrystal.h>
-
-// LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-// const int colums = 20; /// have to be 16 or 20
-// const int rows = 4;    /// have to be 2 or 4
-
-// int lcdindex = 0;
-// int line1[colums];
-// int line2[colums];
-
 int audioInPin = 0;
 int audioOutPin = 10;
 int ledPin = 13;
@@ -21,23 +10,6 @@ int realstatebefore = LOW;
 int filteredstate = LOW;
 int filteredstatebefore = LOW;
 
-///////////////////////////////////////////////////////////
-// The sampling frq will be 8928 on a 16 mhz             //
-// without any prescaler etc                             //
-// because we need the tone in the center of the bins    //
-// you can set the tone to 496, 558, 744 or 992          //
-// then n the number of samples which give the bandwidth //
-// can be (8928 / tone) * 1 or 2 or 3 or 4 etc           //
-// init is 8928/558 = 16 *4 = 64 samples                 //
-// try to take n = 96 or 128 ;o)                         //
-// 48 will give you a bandwidth around 186 hz            //
-// 64 will give you a bandwidth around 140 hz            //
-// 96 will give you a bandwidth around 94 hz             //
-// 128 will give you a bandwidth around 70 hz            //
-// BUT remember that high n take a lot of time           //
-// so you have to find the compromice - i use 48         //
-///////////////////////////////////////////////////////////
-
 float coeff;
 float Q1 = 0;
 float Q2 = 0;
@@ -48,7 +20,7 @@ float target_freq = 744.0;
 float n = 48.0;
 int testData[48];
 
-int nbtime = 20; /// ms noise blanker
+int nbtime = 6; /// ms noise blanker
 
 long starttimehigh;
 long highduration;
@@ -63,6 +35,8 @@ int wpm;
 
 int charcount = 0;
 
+boolean wabun = true;
+
 void setup()
 {
   int k;
@@ -75,14 +49,6 @@ void setup()
 
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-
-  // lcd.clear();
-  // lcd.begin(colums, rows);
-  // for (int index = 0; index < colums; index++)
-  // {
-  //   line1[index] = 32;
-  //   line2[index] = 32;
-  // }
 }
 
 void loop()
@@ -139,7 +105,7 @@ void loop()
           printascii(32);
           if (60 < charcount)
           {
-            Serial.println(wpm);
+            Serial.println();
             charcount = 0;
           }
         }
@@ -165,6 +131,11 @@ void loop()
         wpm = (wpm + (1200 / ((highduration) / 3))) / 2;
       }
     }
+  }
+
+  if ((millis() - startttimelow) > (highduration * 6))
+  {
+    decode();
   }
 
   // updateinfolinelcd();
